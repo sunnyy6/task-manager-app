29da51a93f473dd0b5ee1450b6d12c81a91b0ee2
sunny <karthiksunny066@gmail.com>
2023-10-18 19:45:12 +0530

    spread-operator practice

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "../src/components/App";
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
+
+const citrus = ["lime", "lemon", "orange"];
+const fruits = ["Apple", "Banana", "Coconut", "mango", ...citrus];
+const fullName = {
+  fName: "James",
+  lName: "Bond",
+};
+
+const user = {
+  id: 1,
+  email: "jamesbond007@gmail.com",
+  ...fullName,
+  age: 27,
+};
+console.log(fruits);
+console.log(user);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
